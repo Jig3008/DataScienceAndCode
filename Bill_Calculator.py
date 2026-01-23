45877b5df130ae975cd990b6951bc4c45c7ba7a1
Jigar Joshi <jigar.updates@gmail.com>
2026-01-23 22:42:24 +0100

    Delete Bill_Calculator.py

--- Bill_Calculator.py
+++
@@ -1,8 +0,0 @@
-#Bill Calculator Project
-
-print('Welcome to the tip calculator!')
-billAmount = float(input('What was the total bill? $'))
-tipPercentage = float(input('How much tip would you like to give? 10, 12, or 15? '))
-noOfPeople = int(input('How many people to split the bill? '))
-perPersonAmount = round((billAmount + ((billAmount * tipPercentage))/100)/noOfPeople,2)
-print(f'Each person should pay: ${perPersonAmount}')
